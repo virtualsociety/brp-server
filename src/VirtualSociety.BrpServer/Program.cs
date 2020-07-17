ba3d313b2f35f8ccde2ca652f10b29c1cae8e28e
sjefvanleeuwen <@sjefvanleeuwen>
2020-07-17 09:55:37 +0200

    User Secrets

--- src/VirtualSociety.BrpServer/Program.cs
+++ src/VirtualSociety.BrpServer/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,13 @@ namespace Brp.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostContext, config) =>
+            {
+                var env = hostContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                config.AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+            })
+
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
